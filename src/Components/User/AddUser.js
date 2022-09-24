b559f675cf345de87967ff9ff29d9b98b2398eb0
KAD105 <kazem_abdi@yahoo.com>
2022-09-24 08:44:46 +0200

    add: AddUser component

--- src/Components/User/AddUser.js
+++ src/Components/User/AddUser.js
@@ -1,17 +1,23 @@
-import styles from "./AddUser.module.css";
+import classes from "./AddUser.module.css";
+import Card from "../UI/Card";
+
 const AddUser = () => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="input"></input>
+    <div>
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input id="username" type="input"></input>
 
-      <label htmlFor="age"> Age</label>
-      <input id="age" type="number"></input>
-      <button type="submit">Add User</button>
-    </form>
+          <label htmlFor="age"> Age</label>
+          <input id="age" type="number"></input>
+          <button type="submit">Add User</button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
